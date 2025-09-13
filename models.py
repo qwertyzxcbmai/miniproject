cc11f23a7b7878403ad252b2e46ebbbfc9d9db02
Eva <evacherkashyna9@gmail.com>
2025-09-13 22:50:56 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
+from pydantic import BaseModel, constr
 from flask_sqlalchemy import SQLAlchemy
+
 
 db = SQLAlchemy()
 
@@ -8,3 +10,8 @@ class Product(db.Model):
     category = db.Column(db.String(60), nullable=True)
     price = db.Column(db.Integer, nullable=False)  # цена в грн (целое для простоты)
     description = db.Column(db.String(500), nullable=False)
+    
+class UserRegister(BaseModel):
+    username: constr(min_length=3, max_length=50)
+    password: constr(min_length=6)
+    country: str
